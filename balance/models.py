ecc2514c5e311555ccc528d7682676cac2afa336
Rocío <rocionintendo29@gmail.com>
2023-09-26 21:40:00 +0200

    feat: enlazamos pagina de movimientos

--- balance/models.py
+++ balance/models.py
@@ -1,30 +1,56 @@
 import csv
+from datetime import date
+
 from . import RUTA_FICHERO
+
+
 class Movimiento:
     def __init__(self, fecha, concepto, tipo, cantidad):
-        self.fecha = fecha
+
+        self.errores = []
+
+        try:
+            self.fecha = date.fromisoformat(fecha)
+        except ValueError:
+            self.fecha = None
+            self.errores.append(f'La fecha {fecha} no es una fecha válida')
         self.concepto = concepto
         self.tipo = tipo
         self.cantidad = cantidad
 
+    @property
+    def has_errors(self):
+        return len(self.errores) > 0
+
     def __str__(self):
-        return f'{self.fecha} | {self.concepto} | {self.tipo} | {self.cantidad}'
-        
+        return f'{self.fecha}\t{self.concepto}\t{self.tipo}\t{self.cantidad}\n'
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class ListaMovimientos:
     def __init__(self):
         self.lista_movimientos = []
 
     def leer_desde_archivo(self):
-        self.lista_movimientos= []
-        with open (RUTA_FICHERO, 'r') as fichero:
+        self.lista_movimientos = []
+        with open(RUTA_FICHERO, 'r') as fichero:
             reader = csv.DictReader(fichero)
             for fila in reader:
-                self.lista_movimientos.append(fila)
+                movimiento = Movimiento(
+                    fila['fecha'],
+                    fila['concepto'],
+                    fila['ingreso_gasto'],
+                    fila['cantidad']
+                )
+                self.lista_movimientos.append(movimiento)
 
     def __str__(self):
         result = ''
         for mov in self.lista_movimientos:
             result += f'\n{mov}'
         return result
-        
+
+    def __repr__(self):
+        return self.__str__()
--- balance/views.py
+++ balance/views.py
@@ -3,32 +3,39 @@ from flask import render_template
 from . import app
 from .models import ListaMovimientos
 
+
 @app.route('/')
 def home():
     """
-    Muestra la lista de movimientos cargados
+    Muestra la lista de movimientos cargados.
     """
     lista = ListaMovimientos()
     lista.leer_desde_archivo()
-    return render_template('inicio.html', movs=lista.lista_movimientos)
+
+    return render_template(
+        'inicio.html',
+        movs=lista.lista_movimientos)
+
 
 @app.route('/nuevo')
 def add_movement():
     """
     Crea un movimiento nuevo y lo guarda en el archivo CSV
     """
-    return 'Agrega un movimiento nuevo'
+    return 'Agregar nuevo movimiento'
+
 
 @app.route('/modificar')
 def update():
     """
-    Nos permite editar los datos de un movimiento creado previamente
+    Permite editar los datos de un movimiento creado previamente.
     """
     return 'Actualizar movimiento'
+
 
 @app.route('/borrar')
 def delete():
     """
-    Borra un movimiento existente
+    Borra un movimiento existente.
     """
     return 'Borrar movimiento'
